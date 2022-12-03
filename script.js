2074a648b8e9b2dcb224e542451d302fab1e198c
karguello90 <karguello90@gmail.com>
2022-12-03 09:25:29 -0500

    adding JavaScript timer,questions,litseners

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-let generateBtn = document.querySelector("#start");
+mainPage = document.getElementById("Questions");
+gameStart = document.getElementById("Start-Button").addEventListener("click", gameStart);
+
+
+//One minute timer
+const startingTime = 1;
+//60 seconds timer
+let time = startingTime * 60;
+let timeInterval;
+//Array for quiz questions
+let quizQuestions = [
+    {
+        question:
+    },
+    {
+        question:
+    }
+]
+
+function gameStart() {
+    timeInterval = setInterval (timeStart, 1000);
+    questionBegin();
+}
+
+function timeStart() {
+    if 
+}
