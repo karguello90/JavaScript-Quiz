c2a6bd89618f5aeaa32be1ab328fa2859f389891
karguello90 <karguello90@gmail.com>
2022-12-03 22:02:37 -0500

    added timer, updating javascript

--- script.js
+++ script.js
@@ -1,29 +1,5 @@
-let introContainerEl = document.getElementById("start-questions");
-let questionsContainerEl = document.getElementById("questions-container");
-let endingContainerEl = document.getElementById("ending-container");
-let highscoresContainerEl = document.getElementById("highscore-container");
+let introContainer = document.getElementById("start-questions");
+let questionsContainer = document.getElementById("questions-container");
+let endingContainer = document.getElementById("ending-container");
+let highscoresContainer = document.getElementById("highscore-container");
 
-let 
-let score = 0;
-let timeLeft;
-let gameOver
-timerEl.innerText = 0;
-
-let topScores = [];
-
-//Array for quiz questions
-let quizQuestions = [
-    {
-        question: test question
-        answer: test answer
-        choices: [{choice '1. 1st choice'}]
-
-let gameStart = function() {
-    introContainerEl.classList.add('hide');
-    introContainerEl.classList.remove('show');
-    questionsContainerEl.classList.remove('hide');
-    questionsContainerEl.classList.add('show');
-    timeOn()
-    qeuestionOn()
-}
-    
